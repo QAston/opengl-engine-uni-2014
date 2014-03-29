2f9b18b1a0d8bb8f04c6a73e5d8b7a5a45ac1963
Maciej Sypek <maciej.sypek92@gmail.com>
2014-03-29 17:44:00 +0100

    Dodano funkcję glutSpecialInput, potrzebną do obsługi klawiszy strzałek

--- main.cpp
+++ main.cpp
@@ -5,12 +5,16 @@
 void keyboard(unsigned char key, int x, int y);
 void display(void);
 
+/* Function handling arrow keys */
+void specialInput(int key, int x, int y);
+
 
 int main(int argc, char** argv)
 {
   glutInit(&argc, argv);
   glutCreateWindow("GLUT Test");
   glutKeyboardFunc(&keyboard);
+  glutSpecialFunc(&specialInput);
   glutDisplayFunc(&display);
   glutMainLoop();
 
@@ -29,17 +33,42 @@ void keyboard(unsigned char key, int /*x*/, int /*y*/)
 }
 
 
+void specialInput(int key, int x, int y)
+{
+  switch(key)
+  {
+    case GLUT_KEY_UP:
+			//do something here
+      break;
+    case GLUT_KEY_DOWN:
+      exit(EXIT_SUCCESS);
+      break;
+    case GLUT_KEY_LEFT:
+      break;
+    case GLUT_KEY_RIGHT:
+      break;
+  }
+			
+  glutPostRedisplay();
+} 
+
 void display()
 {
   glClear(GL_COLOR_BUFFER_BIT);
 
-  glColor3f(1.0f, 0.0f, 0.0f);
 
+    glColor3f(1.0f, 0.0f, 0.0f);
   glBegin(GL_POLYGON);
-    glVertex2f(-0.5f, -0.5f);
-    glVertex2f( 0.5f, -0.5f);
-    glVertex2f( 0.5f,  0.5f);
-    glVertex2f(-0.5f,  0.5f);
+    glVertex3f(-0.5f, -0.5f, 0.0f);
+    glVertex3f( 0.5f, -0.5f, 0.0f);
+    glVertex3f( 0.5f,  0.5f, 0.0f);
+    glVertex3f(-0.5f,  0.5f, 0.0f);
+
+	glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(-0.5f, -0.5f, 1.0f);
+    glVertex3f( 0.5f, -0.5f, 1.0f);
+    glVertex3f( 0.5f,  0.5f, 1.0f);
+    glVertex3f(-0.5f,  0.5f, 1.0f);
   glEnd();
 
   glFlush();
