8a13ea5f645a083540ae0b663a33fabf77b722d2
QAston <qaston@gmail.com>
2014-03-30 14:38:33 +0200

    Enable double buffering

--- main.cpp
+++ main.cpp
@@ -1,27 +1,28 @@
 #include <stdlib.h>
 #include <GL/glut.h>
-#include <GL/gl.h>
 
 void keyboard(unsigned char key, int x, int y);
 void display(void);
-
-/* Function handling arrow keys */
+void reshape (int w, int h);
 void specialInput(int key, int x, int y);
 
 
 int main(int argc, char** argv)
 {
+  glutInitWindowSize(800, 600);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
   glutInit(&argc, argv);
-  glutCreateWindow("GLUT Test");
+  /*int mainWindow = */glutCreateWindow("RW3");
   glutKeyboardFunc(&keyboard);
   glutSpecialFunc(&specialInput);
   glutDisplayFunc(&display);
+  glEnable(GL_DEPTH_TEST);
   glutMainLoop();
 
   return EXIT_SUCCESS;
 }
 
-
+// checks ascii input
 void keyboard(unsigned char key, int /*x*/, int /*y*/)
 {
   switch (key)
@@ -29,47 +30,57 @@ void keyboard(unsigned char key, int /*x*/, int /*y*/)
     case '\x1B':
       exit(EXIT_SUCCESS);
       break;
+    case ' ':
+      break;
   }
 }
 
-
-void specialInput(int key, int x, int y)
+// checks raw keyboard input
+void specialInput(int key, int /*x*/, int /*y*/)
 {
   switch(key)
   {
     case GLUT_KEY_UP:
-			//do something here
       break;
     case GLUT_KEY_DOWN:
-      exit(EXIT_SUCCESS);
       break;
     case GLUT_KEY_LEFT:
       break;
     case GLUT_KEY_RIGHT:
       break;
+    default:
+        return;
   }
-			
+
   glutPostRedisplay();
-} 
+}
 
 void display()
 {
-  glClear(GL_COLOR_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-
+  glBegin(GL_LINE_LOOP);
     glColor3f(1.0f, 0.0f, 0.0f);
-  glBegin(GL_POLYGON);
     glVertex3f(-0.5f, -0.5f, 0.0f);
     glVertex3f( 0.5f, -0.5f, 0.0f);
     glVertex3f( 0.5f,  0.5f, 0.0f);
     glVertex3f(-0.5f,  0.5f, 0.0f);
-
-	glColor3f(0.0f, 1.0f, 0.0f);
-    glVertex3f(-0.5f, -0.5f, 1.0f);
-    glVertex3f( 0.5f, -0.5f, 1.0f);
-    glVertex3f( 0.5f,  0.5f, 1.0f);
-    glVertex3f(-0.5f,  0.5f, 1.0f);
+  glEnd();
+  glBegin(GL_LINE_LOOP);
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glVertex3f(-0.5f, -0.5f, -1.0f);
+    glVertex3f( 0.5f, -0.5f, -1.0f);
+    glVertex3f( 0.5f,  0.5f, -1.0f);
+    glVertex3f(-0.5f,  0.5f, -1.0f);
   glEnd();
 
-  glFlush();
+  glutSwapBuffers();
+}
+
+void reshape (int w, int h)
+{
+   /*glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+   glMatrixMode (GL_PROJECTION);
+   glLoadIdentity ();
+   gluOrtho2D (0.0, (GLdouble) w, 0.0, (GLdouble) h);*/
 }
