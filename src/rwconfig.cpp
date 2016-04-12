b0bc8f0f8bc84e999db8fecdc9a9e0bce22067e3
QAston <qaston@gmail.com>
2016-04-12 12:24:05 +0200

    fixes to cmake: default config values, correct include deps.

--- src/rwconfig.cpp
+++ src/rwconfig.cpp
@@ -1,9 +1,10 @@
 #include "rwconfig.h"
 
-// assume data dir is inside working dir, needs to run from workdir
+
 std::string resourcePath(const std::string & p)
 {
-    return "data/" + p;
+    // assume data dir is inside working dir, needs to run from workdir
+    //return "data/" + p;
     //when starting from build dir could also use this as a working dir
-    //return "../data/" + p;
+    return "../data/" + p;
 }
