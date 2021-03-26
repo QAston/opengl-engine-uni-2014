025b1b2834b6adb247c7793c43cd2a6daf2ee568
QAston <qaston@gmail.com>
2021-03-26 14:18:44 +0000

    Update the build and the build instructions

---
+++ README.md
@@ -0,0 +1,27 @@
+## opengl engine uni 2014
+
+An opengl engine (old pipeline rendering) project I co-made for a university course in 2014. Right now mostly used as a demo of how to use cmake and vcpkg.
+
+### Building with vcpkg and cmake
+
+1. Update submodules
+```
+git submodule init
+git submodule update
+```
+2. Build with cmake (msvc)
+```
+# in msvc developer prompt
+mkdir build-msvc
+cd build-msvc
+cmake -DVCPKG_OVERLAY_PORTS=../port_overlays -DVCPKG_FEATURE_FLAGS=versions,manifests -DVCPKG_TARGET_TRIPLET=x64-windows -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake ..
+# build the solution in visual studio
+```
+3. Build with cmake (mingw)
+```
+# in msys2 mingw shell
+mkdir build-mingw
+cd build-mingw
+cmake -DVCPKG_OVERLAY_PORTS=../port_overlays -DVCPKG_FEATURE_FLAGS=versions,manifests -DVCPKG_TARGET_TRIPLET=x64-mingw-dynamic -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake -GNinja ..
+ninja
+```
--- port_overlays/freealut/portfile.cmake
+++ port_overlays/freealut/portfile.cmake
@@ -1,25 +1,33 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO vancegroup/freealut
-    REF fc814e316c2bfa6e05b723b8cc9cb276da141aae
+    REPO cesarizu/freealut
+    REF e7dc5c297e8223a85f9b848c0ff56970db6344b6
+    PATCHES cmake_builds.patch unix_headers.patch 
     HEAD_REF master
-    SHA512 046990cc13822ca6eea0b8e412aa95a994b881429e0b15cefee379f08bd9636d4a4598292a8d46b30c3cd06814bfaeae3298e8ef4087a46eede344f3880e9fed
+    SHA512 1e4e347de70094d0288c9a11ffedbbd3246d99403c2199c692c1bdd5f8ddfd31354dc597c137ce99ab59acc50657007e47153735e189a13152fdc2cef434c09e
 )
+
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" ALUT_BUILD_STATIC)
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" ALUT_BUILD_DYNAMIC)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     DISABLE_PARALLEL_CONFIGURE
-    OPTIONS -DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF
+    OPTIONS -DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF -DBUILD_STATIC=${ALUT_BUILD_STATIC} -DBUILD_DYNAMIC=${ALUT_BUILD_DYNAMIC}
 )
 
 vcpkg_install_cmake()
-#vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/freealut TARGET_PATH share/freealut)
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/freealut TARGET_PATH share/freealut)
 vcpkg_fixup_pkgconfig()
 vcpkg_copy_pdbs()
 
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share
-                     ${CURRENT_PACKAGES_DIR}/debug/include
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share"
+                    "${CURRENT_PACKAGES_DIR}/debug/include"
  )
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
 
 file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
