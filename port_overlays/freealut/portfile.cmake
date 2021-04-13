0fbe321efdd66eebd0cc62782b964c4d0e7b986d
QAston <qaston@gmail.com>
2021-04-13 13:18:47 +0100

    Update the alut port

--- port_overlays/freealut/portfile.cmake
+++ port_overlays/freealut/portfile.cmake
@@ -1,20 +1,21 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO cesarizu/freealut
-    REF e7dc5c297e8223a85f9b848c0ff56970db6344b6
-    PATCHES cmake_builds.patch unix_headers.patch 
+    REPO vancegroup/freealut
+    REF fc814e316c2bfa6e05b723b8cc9cb276da141aae
+    SHA512 046990cc13822ca6eea0b8e412aa95a994b881429e0b15cefee379f08bd9636d4a4598292a8d46b30c3cd06814bfaeae3298e8ef4087a46eede344f3880e9fed
     HEAD_REF master
-    SHA512 1e4e347de70094d0288c9a11ffedbbd3246d99403c2199c692c1bdd5f8ddfd31354dc597c137ce99ab59acc50657007e47153735e189a13152fdc2cef434c09e
+    PATCHES
+        cmake_builds.patch
+        unix_headers.patch 
 )
-
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "static" ALUT_BUILD_STATIC)
-string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" ALUT_BUILD_DYNAMIC)
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     DISABLE_PARALLEL_CONFIGURE
-    OPTIONS -DBUILD_EXAMPLES=OFF -DBUILD_TESTS=OFF -DBUILD_STATIC=${ALUT_BUILD_STATIC} -DBUILD_DYNAMIC=${ALUT_BUILD_DYNAMIC}
+    OPTIONS 
+        -DBUILD_EXAMPLES=OFF
+        -DBUILD_TESTS=OFF
 )
 
 vcpkg_install_cmake()
@@ -28,6 +29,14 @@ file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share"
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
+if(EXISTS "${CURRENT_PACKAGES_DIR}/bin/freealut-config")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin/freealut-config")
+endif()
+
+if(EXISTS "${CURRENT_PACKAGES_DIR}/debug/bin/freealut-config")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/bin/freealut-config")
 endif()
 
 file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
